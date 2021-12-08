ac6e52931660aae527fa994c6313ce1f1ea641fe
Joonas Kelahaara <jolle.kela@gmail.com>
2021-12-08 21:45:30 +0200

    parametreinä react appin kautta tietokantaan

--- index.php
+++ index.php
@@ -6,27 +6,21 @@ $json = json_decode(file_get_contents('php://input'));
 
 $fname = filter_var($json->fname, FILTER_SANITIZE_STRING);
 $lname = filter_var($json->lname, FILTER_SANITIZE_STRING);
-$user = filter_var($json->uname, FILTER_SANITIZE_STRING);
+$uname = filter_var($json->uname, FILTER_SANITIZE_STRING);
 $passwd = filter_var($json->passwd, FILTER_SANITIZE_STRING);
 
 try {
-    $db = new PDO('mysql:host=localhost;dbname=tietoturvadb', 'root', '');
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $db = createDbConnection();
 
     createTable($db);
 
-    $user = filter_var($user, FILTER_SANITIZE_STRING);
-
-    $sql = "SELECT * FROM user WHERE uname = ?";
+    $sql = "INSERT INTO user VALUES(?,?,?,?)";
 
     $prepared = $db->prepare($sql);
-    $prepared->bindParam(1 , $user);
 
-    $prepared->execute();
+    $prepared->execute(array($fname, $lname, $uname, $passwd));
 
-    foreach($prepared as $row){
-        echo $row['fname'];
-    }
+    echo "Added user $uname";
 
 } catch(PDOException $e) {
     echo '<br>'.$e->getMessage();
